58dac2618bec778dfcdfd9da93e310899da05faf
Dilesh <drj855@mun.ca>
2019-01-09 23:18:53 -0330

    first change

---
+++ in-the-end.py
@@ -0,0 +1,22 @@
+import winsound as play
+a=440
+b=247
+c=262
+d=294
+e=330
+f=349
+g=392
+
+t2=2000
+t1=1000
+"""In the end intro"""
+
+play.Beep(d,t1)
+play.Beep(a,t1)
+play.Beep(a,t1)
+play.Beep(f,t1)
+play.Beep(e,t1)
+play.Beep(e,t1)
+play.Beep(e,t1)
+play.Beep(e,500)
+play.Beep(f,500)
